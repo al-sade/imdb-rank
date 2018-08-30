91e0793824832d03fd0af93587fa2142e6051cdc
Al Sade <alsade15@gmail.com>
2018-08-30 16:39:37 +0300

    design adjustment

--- src/App.vue
+++ src/App.vue
@@ -3,38 +3,47 @@
     <el-container>
       <el-main>
         <el-row type="flex" justify="center">
-          <el-input placeholder="Please input" v-model="input" class="input-with-select">
+          <el-input placeholder="Search for Movie / TV Show" v-model="input" class="input-with-select">
             <el-button slot="append" icon="el-icon-search" @click="getData"></el-button>
           </el-input>
         </el-row>
-        <el-collapse-transition>
+        <transition name="el-fade-in-linear">
 
-          <el-row class="grid-content desc" v-if="result.Title" v-loading.fullscreen.lock="loading">
+          <el-row class="desc" v-if="result.Title" align="middle" v-loading.fullscreen.lock="loading">
             <el-col :span="8">
               <div class="img-box" :style="{ backgroundImage: `url('${result.Poster}')` }">
               </div>
             </el-col>
-            <el-col :span="16">
+            <el-col :span="16" style="padding-right: 15px;">
               <h1>{{ result.Title }}</h1>
               <div>
-                <el-rate v-model="result.imdbRating" :max="10" disabled></el-rate>
+                <el-rate v-model="parsedRank" :max="10" disabled></el-rate>
+                <div style="margin: 5px 0;">{{ result.imdbRating }} | {{ result.imdbVotes }} votes  </div>
               </div>
-              <div style="margin: 5px 0;">{{ result.imdbRating }} | {{ result.imdbVotes }}  </div>
-              <div>{{ result.Runtime }} | {{ result.Genre }} | {{ result.Year }}</div>
+              <div class="subtext">{{ result.Runtime }} | {{ result.Genre }} | {{ result.Year }}</div>
               <hr>
               <div>{{ result.Plot }}</div>
-
-              <div><h3>Actors:</h3> {{ result.Actors }}</div>
-              <div><h3>Director:</h3> {{ result.Director }}</div>
-              <div><h3>Writer:</h3> {{ result.Writer }}</div>
+              <div v-for="field in ['Actors','Director','Writer']" v-if="itemExists(result[field])">
+                <h3>{{ field }}:</h3> {{ result[field] }}
+              </div>
               <hr>
-              <div><h4>Language:</h4> {{ result.Language }}</div>
-              <div><h4>Type:</h4> {{ result.Type }}</div>
-              <div><h4>Awards:</h4> {{ result.Awards }}</div>
-              <div><h4>Website:</h4> <a :href="result.Website" target="_blank">{{ result.Website }}</a></div>
+              <div v-for="field in ['Language', 'Type', 'Awards']" v-if="itemExists(result[field])">
+                <h4>{{ field }}:</h4> {{ result[field] }}
+              </div>
+              <div v-if="itemExists(result.Website)">
+                <h4>Website:</h4> <a :href="result.Website" target="_blank">{{ result.Website }}</a>
+              </div>
             </el-col>
           </el-row>
-        </el-collapse-transition>
+
+          <el-row v-if="errorMessage" class="desc">
+            <el-alert
+              :title="errorMessage"
+              type="warning"
+              show-icon>
+            </el-alert>
+          </el-row>
+        </transition>
       </el-main>
     </el-container>
   </div>
@@ -47,8 +56,7 @@
     name: 'app',
     data() {
       return {
-        input: 'titanic',
-        value5: 6.7,
+        input: '',
         baseUrl: 'http://www.omdbapi.com/',
         apiKey: '9e7c06f2',
         result: {},
@@ -79,6 +87,14 @@
             this.result = {};
             this.loading = false;
           });
+      },
+      itemExists(value) {
+        return value !== 'NA';
+      }
+    },
+    computed: {
+      parsedRank() {
+        return parseFloat(this.result.imdbRating);
       }
     }
   }
@@ -92,37 +108,40 @@
 
   .desc {
     width: 60vw;
-    margin: auto;
-    margin-top: 30px;
-    padding: 0px;
+    margin: 30px auto;
+    padding: 0;
+    border-radius: 10px;
+    box-shadow: 0 0 10px #eee;
   }
-
-  .box-header {
-    background-color: #444;
-    color: #FFF;
+  @media screen and (max-width: 600px) {
+    .desc{
+      width: 75vw !important;
+    }
   }
-
   .el-input {
     width: 440px;
   }
 
   .img-box {
-    min-height: 350px;
+    height: 450px;
     margin: 0 15px;
     background-size: contain;
     background-position: center center;
     background-repeat: no-repeat;
   }
 
-  .el-cols {
-    padding-top: 1rem;
-    padding-bottom: 1rem;
-    min-height: 1px;
-  }
-
   h1, h2, h3, h4, h5, h6 {
     display: -webkit-inline-box;
-    margin: 10px 0px 5px;
+    margin: 10px 0 5px;
   }
 
+  hr {
+    border: 0;
+    height: 1px;
+    background-image: linear-gradient(to right, rgba(0, 0, 0, 0), rgb(229, 221, 220), rgba(0, 0, 0, 0));
+  }
+
+  .subtext {
+    font-size: 0.75rem;
+  }
 </style>
